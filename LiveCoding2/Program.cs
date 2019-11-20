b1a5875328469c085c5c38c931acae292ba776d3
Bridgette Kennedy <bridgetteakennedy@gmail.com>
2019-11-19 19:17:40 -0500

    First commit didn't push all files

---
+++ LiveCoding2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LiveCoding2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                        {
+                string longstring = "bdsagbgagggaaatttyyyyau";
+                int i = 0;
+                int count = 0;
+                int count2 = 0;
+                
+                foreach (char character in longstring)
+                {
+                    char x = longstring[i];
+                    if (longstring[i] == longstring[i + 1] && i<longstring.Length-1){
+                        count++;
+                        i++;
+
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                    count2 = count;
+                    if (count2 > count){
+                        Console.WriteLine(longstring[i]);
+                    }
+                }
+                Console.WriteLine($"Welp Count2:{count2} Count: {count}");
+            }
+
+
+        }
+    }
+}
